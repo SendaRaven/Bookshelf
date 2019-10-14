e6df0d77ce65aedda70c91a4e50f61d71bf97025
SendaRaven <42833205+SendaRaven@users.noreply.github.com>
2019-10-15 00:00:44 +0200

    extracted server and database to www like in express-generator

--- app.js
+++ app.js
@@ -1,33 +1,42 @@
 'use strict';
 
 const express = require('express');
-
 const app = express();
 
+const path = require('path')
 const bookRouter = require('./routes/bookRouter');
 const usersRouter = require('./routes/userRouter');
-const auth = require('./middleware/auth')
+//const auth = require('./middleware/auth')
 const errorMiddleware = require('./middleware/error');
-const mongoose = require('mongoose');
-const port = 3000;
+// const port = 3000;
 
-mongoose.connect('mongodb://127.0.0.1:27017/bookshelf', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-})
+// const mongoose = require('mongoose');
 
-const db = mongoose.connection;
-// attach listeners to the db connection
-db.on('error', console.error)
+// mongoose.connect('mongodb://127.0.0.1:27017/bookshelf', {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true
+// })
 
-db.once('open', function () {
-  console.log(`We're connected over http://127.0.0.1:27017/bookshelf!`);
+// const db = mongoose.connection;
+// // attach listeners to the db connection
+// db.on('error', console.error)
 
-})
+// db.once('open', function () {
+//   console.log(`We're connected over http://127.0.0.1:27017/bookshelf!`);
 
-app.listen(port, () => { console.log(`Express is running on port ${port}!`) })
+// })
+
+// app.listen(port, () => { console.log(`Express is running on port ${port}!`) })
+
+/*
+** serves all the static files in the /public directory in the project root
+*/
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/api/user/', express.json(), usersRouter, errorMiddleware.handler);
 app.use('/api/books/', express.json(), bookRouter, errorMiddleware.handler);
 
+
+module.exports = app;
