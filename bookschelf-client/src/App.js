b1ae086b3b96698f4987ff3beea57a06fb00d94b
SendaRaven <r.sahlmann@gmx.de>
2019-10-23 16:24:41 +0200

    using antd UI, implemented basic Browser Router and Nav

--- bookschelf-client/src/App.js
+++ bookschelf-client/src/App.js
@@ -1,12 +1,64 @@
 import React from 'react';
-import './App.css';
+import { Col, Row, Layout } from 'antd'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import "./App.css"
+import Dashboard from '../src/components/Dashboard';
+import Signup from '../src/components/Signup';
+import Login from '../src/components/Login';
+import Home from '../src/components/Home';
+const { Header, Content } = Layout
 
-function App() {
+
+const currentUser = !true;
+
+export default function ButtonAppBar() {
+ 
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (!currentUser) {
+      this.props.history.push("/login")
+    } else {
+      this.props.history.push("/dashboard")
+    }
+
+  };
+
+
   return (
-    <div className="App">
+    <div>
+      <Router>
+        <Layout>
+          <Switch>
+            <Header>
+            <Row type="flex" justify="space-between">
+              <Col >
+                <Link to="/">Bookshelf</Link>
+              </Col>
+              <Col>
+              <Link to='/dashboard'>Dashboard</Link>
+              </Col>
+              <Col>
+              <Link to='/signup'>Signup</Link>
+              </Col>
+              <Col>
+               <Link to="/login">Log In</Link>
+
+              </Col>
+            </Row>
+            </Header>
+
+          </Switch>
+          <Content>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/dashboard" component={Dashboard} />
+            </Switch>
+          </Content >
+        </Layout>
+      </Router>
 
     </div>
   );
 }
-
-export default App;
