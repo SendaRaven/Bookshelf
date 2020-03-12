614908ee495eccf3d1e7c26f47960280ca9f7734
SendARaven <r.sahlmann@gmx.de>
2020-03-12 16:23:36 +0100

    added post route for booksearch

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -3,5 +3,6 @@ const Router = express.Router();
 const bookController = require('../controller/bookController')
 
 Router.get('/', bookController.books);
+Router.post('/', bookController.booksearch);
 
 module.exports = Router;
